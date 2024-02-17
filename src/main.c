a6531c2fa73f48922fbf3a757bcada771858c07c
Ryan Buchner <rabuchner@ucdavis.edu>
2024-02-16 17:20:13 -0800

    Add functionality for launching child process

--- src/main.c
+++ src/main.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "../include/child.h"
+
 
 int main(int argc, char** argv) {
+  if (argc < 2) {
+    perror("User program not specified");
+  }
+
   int user_pid = fork();
   if (user_pid == 0) {
-    // Debugger
-  } else if (user_pid > 0) {
     // Forked Process
+    launch_child(argc - 1, argv + 1);
+  } else if (user_pid > 0) {
+    // Debugger
   } else {
     perror("fork() failed");
   }

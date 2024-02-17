a6f91a215d2709620e1113d5b5334f6c0f066a42
Ryan Buchner <rabuchner@ucdavis.edu>
2024-02-16 18:14:13 -0800

    Initialize child process for tracing

--- src/child.c
+++ src/child.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/ptrace.h>
+
 #include "../include/child.h"
 
 
 void launch_child(int argc, char** argv) {
   char *user_program = argv[0];
+
+  if (ptrace(PT_TRACE_ME, 0, 0, 0)) {
+    // errno = 3;
+    perror("PT_TRACE_ME request failed");
+    exit(3);
+  }
+
   execv(user_program, argv);
 }

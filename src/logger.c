b66d62f2269180c726c60db9c9d17a1e8e775b7d
Ryan Buchner <rabuchner@ucdavis.edu>
2024-02-23 10:21:26 -0800

    Add logging functionality

---
+++ src/logger.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/logger.h"
+
+FILE* global_log = NULL;
+
+void open_log() {
+  if (global_log == NULL)
+    global_log = fopen ("log.txt", "w+");
+}
+
+void close_log() {
+  fclose(global_log);
+}
+
+void logger(const char* tag, const char* message) {
+  time_t now;
+  time(&now);
+  fprintf(global_log, "%s [%s]: %s\n", ctime(&now), tag, message);
+}
--- src/main.c
+++ src/main.c
@@ -3,9 +3,11 @@
 #include <unistd.h>
 
 #include "../include/child.h"
+#include "../include/logger.h"
 
 
 int main(int argc, char** argv) {
+  open_log();
   if (argc < 2) {
     perror("User program not specified");
   }
@@ -13,11 +15,16 @@ int main(int argc, char** argv) {
   int user_pid = fork();
   if (user_pid == 0) {
     // Forked Process
+    logger("child_proc", "Child process started");
     launch_child(argc - 1, argv + 1);
+    logger("child_proc", "Child process completed");
   } else if (user_pid > 0) {
     // Debugger
+    logger("debug_proc", "Debug process started");
+    logger("debug_proc", "Debug process completed");
   } else {
     perror("fork() failed");
   }
+  close_log();
   return 0;
 }

206e8eead47cca5790069345fe07c809ba57f15e
Ryan Buchner <rabuchner@ucdavis.edu>
2024-02-24 04:37:13 -0800

    Add mach functionality to read and write to memory

--- include/mach.h
+++ include/mach.h
@@ -1,10 +1,12 @@
+#include <stdint.h>
+
 #include <mach/thread_status.h>
 
 #pragma once
 
 typedef int pid_t;
 typedef unsigned long pc_t;
-
+typedef unsigned long addr_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,6 +15,10 @@ extern "C" {
 arm_thread_state64_t get_state(pid_t pid);
 
 pc_t get_pc(pid_t pid);
+
+void write_mem(pid_t pid, uint8_t *data, unsigned int bytes, addr_t addr);
+
+void read_mem(pid_t pid, uint8_t *buffer, unsigned int bytes, addr_t addr);
 
 #ifdef __cplusplus
 }
--- src/mach.c
+++ src/mach.c
@@ -9,16 +9,26 @@
 #include "../include/logger.h"
 #include "../include/mach.h"
 
-arm_thread_state64_t get_state(pid_t pid){
+task_t get_task(pid_t pid) {
   task_t task;
+  if (task_for_pid(mach_task_self(), pid, &task))
+    perror("task_for_pid() failed");
+
+  char* string;
+  if(asprintf(&string, "Found task=%x for pid=%x\n", task, pid) < 0)
+    perror("asprintf() formatting failed");
+  logger("debug_proc", string);
+  free(string);
+  return task;
+}
+
+arm_thread_state64_t get_state(pid_t pid){
+  task_t task = get_task(pid);
   long thread = 0;
   thread_act_port_array_t thread_list;
   mach_msg_type_number_t thread_count;
-  mach_msg_type_number_t sc = ARM_THREAD_STATE_COUNT;
+  mach_msg_type_number_t sc = ARM_THREAD_STATE64_COUNT;
   arm_thread_state64_t arm_state;
-
-  if (task_for_pid(mach_task_self(), pid, &task))
-    perror("task_for_pid() failed");
 
   if (task_threads(task, &thread_list, &thread_count))
     perror("task_threads() failed");
@@ -27,7 +37,7 @@ arm_thread_state64_t get_state(pid_t pid){
                        ARM_THREAD_STATE64,
                        (thread_state_t)&arm_state,
                        &sc) ||
-      sc != ARM_THREAD_STATE_COUNT)
+      sc != ARM_THREAD_STATE64_COUNT)
     perror("thread_get_state() failed");
   return arm_state;
 }
@@ -42,4 +52,45 @@ pc_t get_pc(pid_t pid) {
   logger("debug_proc", string);
   free(string);
   return pc;
+}
+
+void write_mem(pid_t pid, uint8_t *data, unsigned int bytes, addr_t addr) {
+  task_t task = get_task(pid);
+  kern_return_t error;
+
+  if ((error = vm_protect(task, addr, bytes, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY)))
+    fprintf(stderr, "vm_protect() failed with code= %d\n", error);
+
+  if ((error = vm_write(task, (vm_address_t) addr, (vm_offset_t) data, (mach_msg_type_number_t) bytes)))
+    fprintf(stderr, "vm_write() failed with code= %d\n", error);
+
+  if ((error = vm_protect(task, addr, bytes, FALSE, VM_PROT_READ | VM_PROT_EXECUTE)))
+    fprintf(stderr, "vm_protect() failed with code= %d\n", error);
+
+  for (int i=0; i<bytes; ++i) {
+    char* string;
+    if(asprintf(&string, "Data= %hhx written to Addr= %lx\n", data[i], (vm_address_t) (addr + 1)) < 0)
+      perror("asprintf() formatting failed");
+    logger("debug_proc", string);
+    free(string);
+  }
+}
+
+void read_mem(pid_t pid, uint8_t *buffer, unsigned int bytes, addr_t addr) {
+  get_pc(pid);
+  task_t task = get_task(pid);
+  kern_return_t error;
+  vm_offset_t local_buffer;
+  if ((error = vm_read(task, (vm_address_t) addr, bytes, (vm_offset_t*) &local_buffer, (mach_msg_type_number_t*) &bytes)))
+    fprintf(stderr, "vm_read() failed with code= %d\n", error);
+
+  memcpy(buffer, (const void *) local_buffer, bytes);
+
+  for (int i=0; i<bytes; ++i) {
+    char* string;
+    if(asprintf(&string, "Data= %hhx read from Addr= %lx\n", buffer[i], (vm_address_t) (addr + i)) < 0)
+      perror("asprintf() formatting failed");
+    logger("debug_proc", string);
+    free(string);
+  }
 }

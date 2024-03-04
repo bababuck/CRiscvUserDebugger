23e3263ada24347603489a74f74e0e77cf0518cd
bababuck <92571492+bababuck@users.noreply.github.com>
2024-03-03 20:42:01 -0800

    Add functionality to launch new program from child process

--- include/exit_codes.h
+++ include/exit_codes.h
@@ -3,4 +3,5 @@
 enum exit_code {
   DBG_SUCCESS=0,
   DBG_SYSCALL_FAILURE=1,
+  DBG_ILLEGAL_ARGUMENTS=2
 };
--- src/main.c
+++ src/main.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "../include/child.h"
 #include "../include/exit_codes.h"
 
+
 int main(int argc, char** argv) {
+  if (argc < 2) {
+    perror("User program not specified");
+    exit(DBG_ILLEGAL_ARGUMENTS);
+  }
+
   int user_pid = fork();
   if (user_pid > 0) {
     // Debugger
   } else if (user_pid == 0) {
     // Child Process
+    int child_argc = argc - 1;
+    char** child_argv = argv + 1;
+    launch_child(child_argc, child_argv);
   } else {
     perror("fork() failed");
     exit(DBG_SYSCALL_FAILURE);

e9faeac7dadee21406e74e45a35f145546f77c8b
bababuck <92571492+bababuck@users.noreply.github.com>
2024-03-06 18:04:56 -0800

    Initialize child process for tracing

--- src/child.c
+++ src/child.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/ptrace.h>
+
 #include "../include/child.h"
+#include "../include/exit_codes.h"
 
 
 void launch_child(int argc, char** argv) {
   char *user_program = argv[0];
+
+  if (ptrace(PT_TRACE_ME, 0, 0, 0)) {
+    perror("PT_TRACE_ME request failed");
+    exit(DBG_SYSCALL_FAILURE);
+  }
+
   execv(user_program, argv);
+  perror("execv() failed");
+  exit(DBG_SYSCALL_FAILURE);
 }
